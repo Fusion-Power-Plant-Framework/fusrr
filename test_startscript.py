7b92404cc3b9bbc4ebe1c1b55c448a702a498920
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-06-20 13:35:24 +0100

    Script with pytest
    begins to tidy code and run 1 test

---
+++ test_startscript.py
@@ -0,0 +1,30 @@
+"""Imports Simple Example of reactor, changes location, selects/ generates and colours meshes and outputs a rendered PNG file"""
+import bpy
+import os
+
+def save_image():
+    """Saves render as PNG"""
+    bpy.context.scene.render.filepath = '/home/hannah/blender_venv/Image_name' 
+    bpy.ops.render.render(write_still=1)
+
+bpy.ops.import_scene.gltf(filepath="/home/hannah/Downloads/SimpleExample.gltf")
+
+bpy.context.object.location = (1, 2, 12)
+
+activeobject = bpy.context.scene.objects[1] #1 and 2 refers to mesh LCFS_1 and Winding_pack_1 (print(bpy.data.meshes)) 
+obj1 = bpy.context.scene.objects[2]
+
+mat = bpy.data.materials.new(name="MatName")
+material = bpy.data.materials.new(name="MaterialName")
+
+activeobject.data.materials.append(mat)
+obj1.data.materials.append(material)
+
+
+mat.diffuse_color = (10, 3, 12, 5)
+material.diffuse_color = (1, 2, 13, 4)
+
+save_image()
+
+def test_saveimage():
+    assert os.path.isfile('/home/hannah/blender_venv/Image_name.png') == True
