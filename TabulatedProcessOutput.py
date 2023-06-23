2bff265d783bff13cf29ee6a8a85799d0b7ec303
Miles Murphy <miles.murphy@ukaea.uk>
2023-06-23 16:41:49 +0100

    Extracting  parameters from PROCESS output file

--- TabulatedProcessOutput.py
+++ TabulatedProcessOutput.py
@@ -1,5 +1,13 @@
+"""
+Extracts Data From PROCESS Output file and Stores In Data Class
+
+Works with imported process data file but also allows manual addition of parameter values.
+Currently set to 4 test parameters but can choose any attribute from .DAT file
+
+"""
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Optional
 
 from process.io.mfile import MFile
 from tabulate import tabulate
@@ -9,35 +17,29 @@ from tabulate import tabulate
 class OutputParams:
     rmajor: float
     rminor: float
-    no_of_tfcoils: float
+    n_tf: float
     bigq: float
+    file_name: Optional[str] = None
 
-    def tab(self, set2):
-        paramtable = [
-            ["Output Parameters", "Data Set 1", "Data Set 2"],
-            ["rmajor", self.rmajor, set2.rmajor],
-            ["rminor", self.rminor, set2.rminor],
-            ["no_of_tfcoils", self.no_of_tfcoils, set2.no_of_tfcoils],
-            ["Q", self.bigq, set2.bigq],
-        ]
+    @classmethod
+    def from_file(cls, file_name):
+        mfile_path = Path(
+            file_name
+        )  # insert file path here to PROCESS OUTPUT FILE .DAT
+        mfile = MFile(filename=str(mfile_path))
 
-        output_table = tabulate(paramtable)
-        print(output_table)
+        output_params = list(
+            cls.__annotations__.keys()
+        )  # Creates list to find and remove file_name from mfile data
+        output_params.pop(
+            output_params.index("file_name")
+        )  # Removes file_name to match key values to attributes
 
-
-def get_data(file_name=".DAT file"):
-    mfile_path = Path(file_name)  # insert file path here to PROCESS OUTPUT FILE .DAT
-    mfile = MFile(filename=str(mfile_path))
-    rmajor = mfile.data["rmajor"].get_scan(-1)
-    rminor = mfile.data["rminor"].get_scan(-1)
-    no_of_tfcoils = mfile.data["n_tf"].get_scan(-1)
-    bigq = mfile.data["bigq"].get_scan(-1)
-
-    return rmajor, rminor, no_of_tfcoils, bigq
+        return cls(
+            file_name=file_name,
+            **{param: mfile.data[param].get_scan(-1) for param in output_params}
+        )
 
 
-rmajor, rminor, no_of_tfcoils, bigq = get_data("silly_data.DAT")
-Instance1 = OutputParams(rmajor, rminor, no_of_tfcoils, bigq)
-rmajor, rminor, no_of_tfcoils, bigq = get_data("baseline_2018_MFILE.DAT")
-Instance2 = OutputParams(rmajor, rminor, no_of_tfcoils, bigq)
-Instance1.tab(Instance2)
+b = OutputParams.from_file("baseline_2018_silly_MFILE.DAT")
+print(b)
