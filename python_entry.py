8c1acc3cac98ef94c845c0dc6d5f25bde09d9175
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-06-29 16:44:18 +0100

    Update python_entry.py
    New function for changing object which is called in Save_image func

--- python_entry.py
+++ python_entry.py
@@ -5,8 +5,25 @@ def import_file(pathforfile):
     """Imports gltf file"""
     bpy.ops.import_scene.gltf(filepath= pathforfile)
 
+def object_change(obj_1,obj_2):
+    """Selects active object, creates new meshes and assigns colours
+    obj_x = name/number of desired object"""
+    activeobject = bpy.context.scene.objects[obj_1] #1 and 2 refers to mesh LCFS_1 and Winding_pack_1 (print(bpy.data.meshes)) 
+    obj1 = bpy.context.scene.objects[obj_2]
+
+    mat = bpy.data.materials.new(name="MatName")
+    material = bpy.data.materials.new(name="MaterialName")
+
+    activeobject.data.materials.append(mat)
+    obj1.data.materials.append(material)
+
+
+    mat.diffuse_color = (10, 3, 12, 5)
+    material.diffuse_color = (1, 2, 13, 4)
+
 def save_image(Image_pathname):
     """Saves render as PNG"""
+    object_change(1, 2)
     bpy.context.scene.render.filepath = Image_pathname 
     bpy.ops.render.render(write_still=1)
 
@@ -25,18 +42,5 @@ imagepathname = "/home/blender_venv/Image_name.png"
 import_file(importfilepath)
 
 move_location(1,2,12)
-
-activeobject = bpy.context.scene.objects[1] #1 and 2 refers to mesh LCFS_1 and Winding_pack_1 (print(bpy.data.meshes)) 
-obj1 = bpy.context.scene.objects[2]
-
-mat = bpy.data.materials.new(name="MatName") #creates new mesh
-material = bpy.data.materials.new(name="MaterialName")
-
-activeobject.data.materials.append(mat)
-obj1.data.materials.append(material)
-
-
-mat.diffuse_color = (10, 3, 12, 5) #changes mesh colour
-material.diffuse_color = (1, 2, 13, 4)
 
 save_image(imagepathname )
