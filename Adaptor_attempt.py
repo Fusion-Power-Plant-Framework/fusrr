e3e0c8918cc8a7169ad43d13435ed11d427215b3
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-07-04 11:51:06 +0100

    Update Adaptor_attempt.py
    Changing adapter functions and inputs

--- Adaptor_attempt.py
+++ Adaptor_attempt.py
@@ -1,22 +1,25 @@
 """Adaptor to convert BLUEMIRA and PROCESS parameters to common Reactor class"""
-#(Currently untested WIP)
-from process.io.mfile import MFile
+#(Currently WIP)
 from extract_params import OutputParams
+from dataclasses import asdict
+
+input_data = OutputParams.from_file('baseline_MFILE.DAT')
+input = asdict(input_data)
 
 class BasicParameters:
     """Adapting basic BLUEMIRA or PROCESS data classes to common format"""
     def __init__(self, data_input):
         self.data_input  = data_input
         
-        if self.data_input == MFile: 
-            self.adapt_process()
-        else:
+        if self.data_input == 'BLUEMIRA': 
             self.adapt_bluemira()
+        else:
+            self.adapt_process()
     
     def adapt_process(self):
         """Convert parameters from PROCESS dataclasses to generic format"""
         from Dictionary_Basic import process_param
-        for param_name, param_value in self.data_input.items():
+        for param_name, param_value in input.items():
             generic_name = process_param[param_name]
             setattr(self, generic_name, param_value)
 
@@ -27,7 +30,7 @@ class BasicParameters:
             generic_name = bluemira_param[param_name]
             setattr(self, generic_name, param_value)
 
-input_data = OutputParams.from_file('baseline_MFILE.DAT')
+
 
 adapted_params = BasicParameters(input_data)
 
