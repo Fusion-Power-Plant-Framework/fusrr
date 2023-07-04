25d2f2445848c2683503d4dd7dd7c11154589591
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-07-04 15:05:04 +0100

    Update Dictionary_Basic.py
    spelling

--- Dictionary_Basic.py
+++ Dictionary_Basic.py
@@ -1,4 +1,4 @@
-#Dictionary for generic params for adaptor
+#Dictionary for generic params for adapter
 
 process_param = dict({'rmajor':'rmajor', 'n_tf':'No_TF'})
 bluemira_param = dict({'R_0':'rmajor', 'n_TF':'No_TF'})
