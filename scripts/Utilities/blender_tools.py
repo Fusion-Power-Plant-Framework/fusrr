0f5325ce52e588c610afb2fcf58dfe512c3303ba
Miles Murphy <miles.murphy@ukaea.uk>
2023-07-10 15:33:57 +0100

    Added Tracking function

--- scripts/Utilities/blender_tools.py
+++ scripts/Utilities/blender_tools.py
@@ -4,6 +4,11 @@
 import bpy
 
 
+def empty_obj(x, y, z):
+    """Empty object adder (for camera tracking purposes)"""
+    bpy.ops.object.empty_add(location=(x, y, z))
+
+
 def add_light(x, y, z):
     """adds sunlight object"""
     bpy.ops.object.light_add(type="SUN", location=(x, y, z))
@@ -24,7 +29,7 @@ def camera_fix(camera: str, target: str):
     target : bpy.data.object[""]
     """
 
-    # *Will break if object "camera" or target doesn't exist.
+    # *Will break if object "camera" or "target" doesn't exist.
     constraint = bpy.data.objects[camera].constraints.new(type="TRACK_TO")
     constraint.target = bpy.data.objects[target]
 
