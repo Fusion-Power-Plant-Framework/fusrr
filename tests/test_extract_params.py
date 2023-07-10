91893754a599df088194a640f1eebbb975cc5d43
Miles Murphy <miles.murphy@ukaea.uk>
2023-07-10 10:20:08 +0100

    Updated test_extract_params file - Renamed file - Added docstrings - Updated tests to be more useful

--- tests/test_extract_params.py
+++ tests/test_extract_params.py
@@ -6,18 +6,22 @@ from extract_params import OutputParams
 from process.io.mfile import MFile
 
 
-class Testfilein:
+class TestOutputParams:
+    """Test Class for extract_params"""
+
     def setup_method(self):
-        self.results = (18, 2.9473, 16, 38.923, 7200)
-        self.test = OutputParams(
+        """Setup method that is run when pytest is run"""
+        self.expected_results = (18, 2.9473, 16, 38.923, 7200)
+        self.observed_results = OutputParams(
             rmajor=18, rminor=2.9473, n_tf=16, bigq=38.923, tburn=7200
         )
 
     def test_correct_params(self):
-        for (key, value), result in zip(asdict(self.test).items(), self.results):
-            assert value == pytest.approx(result)
+        """Testing an attritbute of OutputParams to ensure data stored correctly"""
+        assert self.observed_results.n_tf == pytest.approx(16)
 
     def test_correct_params_from_file(self):
+        """Testing the import from MFile, ensuring all data is stored correctly"""
         mfile = MFile(Path(__file__).parent / "test_react.DAT")
         for count, (key, value) in enumerate(asdict(self.test).items()):
             if key != "file_name":
