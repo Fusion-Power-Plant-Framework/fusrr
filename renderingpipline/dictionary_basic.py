4068f1061d36a3516b6e7246a40ea95b63666630
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-07-11 15:06:41 +0100

    Create generalised adaptor for reading parameters from file (#21)
    * Create Adaptor_attempt.py
    
    WIP, starting script for building a common reactor class
    
    * Create Dictionary_Basic.py
    
    Dictionary for trialing two basic parameters
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    Changing adapter functions and inputs
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    renames params, currently output includes original input data as well as generic names
    
    * Update Dictionary_Basic.py
    
    spelling
    
    * Rename Adaptor_attempt.py to Adapter_attempt.py
    
    spelling
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * Create extract_adapt.py
    
    WIP for module to extract + adapt data to generic form
    
    * Delete Adapter_attempt.py
    
    replaced by extract_adapt.py
    
    * Update extract_adapt.py
    
    Extracts and adapts data from both BLUEMIRA and PROCESS
    
    * Update Dictionary_Basic.py
    
    * Update Dictionary_Basic.py
    
    * Update extract_params.py
    
    Revert to original as created a separate extract_adapt module
    
    * Make the python package and add code quality CI (#18)
    
    * 🙈 Add gitignore
    
    * 📄 Add LICENCE
    
    * ⚗️ Add citation file
    
    * 📦️ Create package
    
    * 🚚 Move scripts to scritps dir
    
    * 📄 Reorder licence names
    
    * 👷 Create CI
    
    * 🐛 Rename ci
    
    * 🔧 Add flake8 config
    
    * ⚗️ Try and fail the CI
    
    * ⚗️ Test 2
    
    * 🎨 Editable because we dont want duplicates acting oddly
    
    * 💬 Rending -> rendering where appropriate
    
    * updating files
    
    Combining adapting codes to get generic output from BM and P
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * Update extract_params.py
    
    Revert to original as created a separate extract_adapt module
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * updating to resolve conflicts
    
    * Updating adaptor - more concise
    
    * Update extract_adapt.py
    
    * Update and rename extract_adapt.py to Adaptor.py
    
    * Updates
    
    * Update Adaptor.py
    
    * Updates, added main_adaptor
    
    * Update to pass tests
    
    * Update to pass tests
    
    * More updates - more concise
    
    * move and rename files
    
    * Update renderingpipline/adaptor.py
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>

---
+++ renderingpipline/dictionary_basic.py
@@ -0,0 +1,4 @@
+"""Dictionary for generic params for adaptor"""
+
+process_param = dict({"rmajor": "rmajor", "No_TF": "n_tf"})
+bluemira_param = dict({"rmajor": "R_0", "No_TF": "n_TF"})
---
+++ scripts/main_adaptor.py
@@ -0,0 +1,19 @@
+"""Executes adaptor.py"""
+import argparse
+
+from renderingpipline.adaptor import OutputParams
+
+
+def main():
+    """Pass in file and call to generate generic output"""
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-fn", "--file_name", help="input file")
+    args = argparser.parse_args()
+    file_name = str(args.file_name)
+    generic_output = OutputParams.from_file(file_name)
+    print("generic_output=", generic_output)
+    return generic_output
+
+
+if __name__ == "__main__":
+    generic_output = main()
