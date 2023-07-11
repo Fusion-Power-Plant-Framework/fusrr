f884b7e51407301718f0164e59ce3aa5df34b6a2
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-07-11 15:06:41 +0100

    Create generalised adaptor for reading parameters from file (#21)
    * Create Adaptor_attempt.py
    
    WIP, starting script for building a common reactor class
    
    * Create Dictionary_Basic.py
    
    Dictionary for trialing two basic parameters
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    Changing adapter functions and inputs
    
    * Update Adaptor_attempt.py
    
    * Update Adaptor_attempt.py
    
    renames params, currently output includes original input data as well as generic names
    
    * Update Dictionary_Basic.py
    
    spelling
    
    * Rename Adaptor_attempt.py to Adapter_attempt.py
    
    spelling
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * Create extract_adapt.py
    
    WIP for module to extract + adapt data to generic form
    
    * Delete Adapter_attempt.py
    
    replaced by extract_adapt.py
    
    * Update extract_adapt.py
    
    Extracts and adapts data from both BLUEMIRA and PROCESS
    
    * Update Dictionary_Basic.py
    
    * Update Dictionary_Basic.py
    
    * Update extract_params.py
    
    Revert to original as created a separate extract_adapt module
    
    * Make the python package and add code quality CI (#18)
    
    * 🙈 Add gitignore
    
    * 📄 Add LICENCE
    
    * ⚗️ Add citation file
    
    * 📦️ Create package
    
    * 🚚 Move scripts to scritps dir
    
    * 📄 Reorder licence names
    
    * 👷 Create CI
    
    * 🐛 Rename ci
    
    * 🔧 Add flake8 config
    
    * ⚗️ Try and fail the CI
    
    * ⚗️ Test 2
    
    * 🎨 Editable because we dont want duplicates acting oddly
    
    * 💬 Rending -> rendering where appropriate
    
    * updating files
    
    Combining adapting codes to get generic output from BM and P
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * Update extract_params.py
    
    Revert to original as created a separate extract_adapt module
    
    * Combining extract_params.py and Adapter_attempt.py
    
    Combining adapting codes to get generic output from BM and P
    
    * updating to resolve conflicts
    
    * Updating adaptor - more concise
    
    * Update extract_adapt.py
    
    * Update and rename extract_adapt.py to Adaptor.py
    
    * Updates
    
    * Update Adaptor.py
    
    * Updates, added main_adaptor
    
    * Update to pass tests
    
    * Update to pass tests
    
    * More updates - more concise
    
    * move and rename files
    
    * Update renderingpipline/adaptor.py
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>

---
+++ renderingpipline/adaptor.py
@@ -0,0 +1,81 @@
+"""
+Takes PROCESS (.DAT) or BLUEMIRA (.json) files and outputs parameters the same format.
+
+"""
+from __future__ import annotations
+
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+from process.io.mfile import MFile
+
+from renderingpipline.dictionary_basic import bluemira_param, process_param
+
+
+def process_file_adaptor(input_data, filepath):
+    """
+    Imports data from mfile and assigns name and value
+
+    Returns
+    -------
+        Dictionary containing generic parameters and their values
+    """
+    variables = {}
+    data_obj = MFile(filename=str(filepath))
+    for name_1 in input_data:
+        variables[name_1] = data_obj.data[process_param[name_1]].get_scan(-1)
+    return variables
+
+
+def bluemira_file_adaptor(input_data, filepath):
+    """
+    Converts generic name into BLUEMIRA parameters to then be reassigned values and names
+
+    Returns
+    -------
+        Dictionary containing generic parameters and their values
+    """
+    variables = {}
+    with open(str(filepath), "r") as fh:
+        data_obj = json.load(fh)
+    for name in input_data:
+        try:
+            variables[name] = data_obj[bluemira_param[name]]["value"]
+        except KeyError:
+            continue
+    return variables
+
+
+@dataclass
+class OutputParams:
+    """DataClass to store generic output parameters
+
+    Returns
+    -------
+        dataclass: listed parameters, their values + file_name which
+        is optional in case of manual input for OutputParams
+    """
+
+    rmajor: float
+    No_TF: float
+    file_name: Optional[str] = None
+
+    @classmethod
+    def from_file(cls, file_name: str) -> OutputParams:
+        """
+        Makes instance of class from file name
+        and assigns values to generic parameters
+        """
+        file_path = Path(file_name)
+        output_names = list(cls.__annotations__.keys())
+        output_names.pop(output_names.index("file_name"))
+
+        if file_name.endswith(".DAT"):
+            parameters = process_file_adaptor(output_names, file_path)
+
+        elif file_name.endswith(".json"):
+            parameters = bluemira_file_adaptor(output_names, file_path)
+
+        return cls(file_name=file_name, **parameters)
