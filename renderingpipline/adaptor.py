bd4de14e1e1270182941ef909e80eeddc45ee5fd
HanBrady <136437302+HanBrady@users.noreply.github.com>
2023-07-11 15:05:00 +0100

    Update renderingpipline/adaptor.py
    Co-authored-by: je-cook <81617086+je-cook@users.noreply.github.com>

--- renderingpipline/adaptor.py
+++ renderingpipline/adaptor.py
@@ -9,8 +9,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
-from Dictionary_Basic import bluemira_param, process_param
 from process.io.mfile import MFile
+
+from renderingpipline.dictionary_basic import bluemira_param, process_param
 
 
 def process_file_adaptor(input_data, filepath):
