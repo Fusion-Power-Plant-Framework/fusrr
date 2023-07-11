56fe9f35b9b6e622be354a3d7322f7ed000844d1
HanBrady <hannah.brady@ukaea.uk>
2023-07-11 13:43:41 +0100

    Updates, added main_adaptor

---
+++ scripts/adaptor.py
@@ -0,0 +1,73 @@
+"""
+Takes PROCESS (.DAT) or BLUEMIRA (.json) files and outputs parameters the same format.
+
+"""
+from __future__ import annotations
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+from process.io.mfile import MFile
+import argparse
+import json
+from Dictionary_Basic import process_param
+from Dictionary_Basic import bluemira_param
+
+def process_file_adaptor(input_data, filepath):
+    """
+    Imports data from mfile and assigns name and value
+    
+    Returns:
+        Dictionary containing generic parameters and their values
+    """
+    variables = {}
+    data_obj = MFile(filename=str(filepath))
+    for name_1 in input_data:
+        variables[name_1] = data_obj.data[process_param[name_1]].get_scan(-1)
+    return variables
+
+def bluemira_file_adaptor(input_data, filepath):
+    """
+    Converts generic name into BLUEMIRA parameters to then be reassigned values and names
+
+    Returns:
+        Dictionary containing generic parameters and their values
+    """
+    blue_out = []
+    variables = {}
+    with open(str(filepath), "r") as fh:
+        data_obj = json.load(fh)
+    for param_name in input_data:
+        blue_out.append(bluemira_param[param_name])
+    for name_1, name_2 in zip(input_data, blue_out):
+        try:
+            variables[name_1] = data_obj[name_2]["value"]
+        except KeyError:
+            continue
+    return variables
+
+@dataclass
+class OutputParams:
+    """DataClass to store generic output parameters
+
+    Returns:
+        dataclass: listed parameters, their values + file_name which is optional in case of manual
+        input for OutputParams
+    """
+    rmajor: float
+    No_TF: float
+    file_name: Optional[str] = None
+
+    @classmethod
+    def from_file(cls, file_name: str) -> OutputParams:
+        """Makes instance of class from file name and assigns values to generic parameters"""
+        file_path = Path(file_name) 
+        output_names = list(cls.__annotations__.keys())
+        output_names.pop(output_names.index("file_name"))
+
+        if file_name.endswith(".DAT"):
+            parameters = process_file_adaptor(output_names, file_path)
+            
+        elif file_name.endswith(".json"):
+            parameters = bluemira_file_adaptor(output_names, file_path)
+
+        return cls(file_name=file_name, **parameters)
