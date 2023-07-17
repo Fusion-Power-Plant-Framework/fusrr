a8c3ac2f5db08c1e95e3f1b6b77f03faccbd2e82
HanBrady <hannah.brady@ukaea.uk>
2023-07-17 16:32:01 +0100

    WIP for adaptor test

---
+++ tests/test_adaptor.py
@@ -0,0 +1,13 @@
+"""test for adaptor"""
+
+from adaptor import OutputParams, bluemira_file_adaptor, process_file_adaptor
+
+# test to see if generic outputs are the same
+# test to see if dictionary is returned
+# check that an error is given if not .DAT or .json
+
+
+def test_from_file():
+    """Test to assert if dictionary is produced"""
+    output = OutputParams.from_file("scripts/baseline_2018_MFILE.DAT")
+    assert type(output) is dict == True
