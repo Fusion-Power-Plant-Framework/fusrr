9c4ff9b33a6d569bfa078f8083cbcd82aa991868
HanBrady <hannah.brady@ukaea.uk>
2023-07-20 10:35:19 +0100

    Updates python_entry

--- scripts/python_entry.py
+++ scripts/python_entry.py
@@ -66,10 +66,18 @@ for path in importfilepaths:
     break
 imagepathname = "render.png"
 
-try:
-    import_file(str(file_import))
-except NameError:
+if Path(file_import).exists():
+    delete_cube()
+    move_camera(15, -20, 20)
+    move_location(1, 2, 12)
+
+    save_image(imagepathname)
+else:
     raise FileNotFoundError(f"No .gltf found in {str(Path.cwd())}")
+# try:
+#     import_file(str(file_import))
+# except NameError:
+#     raise FileNotFoundError(f"No .gltf found in {str(Path.cwd())}")
 delete_cube()
 move_camera(15, -20, 20)
 move_location(1, 2, 12)
