b66b0fa34baf9db9c3aff9f739bd5eedd178af8e
clmould <86794332+clmould@users.noreply.github.com>
2024-04-25 13:21:19 +0100

    Add blanket component (#119)

--- fusrr/reactor/process/components/__init__.py
+++ fusrr/reactor/process/components/__init__.py
@@ -1,3 +1,4 @@
 from fusrr.reactor.process.components.pf_coils import ProcessPFCoils
 from fusrr.reactor.process.components.plasma import ProcessPlasma
 from fusrr.reactor.process.components.vacuum_vessel import ProcessVacuumVessel
+from fusrr.reactor.process.components.blanket import ProcessBlanket
